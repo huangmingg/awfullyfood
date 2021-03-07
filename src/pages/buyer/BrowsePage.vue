8a09b0b311badbd462ff45d56fe46ddd1afd85a1
huangminggg <huangming.low@fundingsocieties.com>
2021-03-07 18:01:32 +0800

    Added dummy pages for buyer

---
+++ src/pages/buyer/BrowsePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Browsing page</div>
+</template>
+
+<script>
+export default {
+name: "Browse"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/buyer/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Home Page</div>
+</template>
+
+<script>
+
+export default {
+  name: "Home",
+  created() {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
