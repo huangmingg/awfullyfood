9a8658821c80b192e18f45e240453a8da26478ab
huangminggg <huangming.low@fundingsocieties.com>
2021-03-07 18:00:03 +0800

    Create RegistrationPage.vue

---
+++ src/pages/seller/RegistrationPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>TODO: Form for role selection + user details</div>
+</template>
+
+<script>
+export default {
+  name: "RegistrationPage"
+}
+</script>
+
+<style scoped>
+
+</style>
