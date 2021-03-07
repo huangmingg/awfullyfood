0a85c68a3d44cfb0c7891dca6ff35846a32fa58d
huangminggg <huangming.low@fundingsocieties.com>
2021-03-07 18:00:31 +0800

    Update main.js
    Added dummy auth guard

--- src/main.js
+++ src/main.js
@@ -1,8 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import { Routing } from './routes'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { authService } from '@/firebase'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(VueRouter)
+Vue.use(IconsPlugin)
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const routing = new VueRouter({
+  routes: Routing,
+  mode: 'history'
+});
+
+routing.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
+  if (requiresAuth && !authService.currentUser) {
+    next('/login')
+  } else {
+    next()
+  }
+})
+
+let app
+authService.onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router: routing,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
