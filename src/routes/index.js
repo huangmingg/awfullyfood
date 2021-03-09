1bfe244d5d14831b45ae4630cd32b7f69099f9d2
huangminggg <huangming.low@fundingsocieties.com>
2021-03-09 11:35:47 +0800

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import { routes } from './routes'
 import { authService } from "@/firebase";
-import { store } from "@/store";
+import { store } from "@/stores";
 
 Vue.use(VueRouter)
 
