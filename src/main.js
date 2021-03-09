3cddbfacd1cd310176ca7395cd0ed3568273c540
huangminggg <huangming.low@fundingsocieties.com>
2021-03-09 11:29:38 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,31 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
-import Vuex from 'vuex'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import { router } from './routes'
+import store from './stores'
 import { authService } from '@/firebase'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
-Vue.use(VueRouter)
 Vue.use(IconsPlugin)
-Vue.use(Vuex)
-
-const store = new Vuex.Store({
-  state: {
-    count: 0
-  },
-  mutations: {
-    increment (state) {
-      state.count++
-    }
-  }
-})
 
 let app
+console.log(router);
 authService.onAuthStateChanged(() => {
   if (!app) {
     app = new Vue({
