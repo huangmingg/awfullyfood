1967ca0260088380423f0e7d4c8c25905cc3be08
huangminggg <huangming.low@fundingsocieties.com>
2021-03-09 11:29:11 +0800

    Created routing module

---
+++ src/routes/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import { routes } from './routes'
+import { authService } from "@/firebase";
+import { store } from "@/store";
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    linkActiveClass: 'is-active',
+    mode: 'history',
+    routes
+})
+
+router.beforeEach((to, from, next) => {
+    // const requiresAuth = to.matched.some(x => x?.meta?.requiresAuth);
+    const userRole = store?.getters?.getRole;
+    const routeRole = to?.meta?.role;
+    if (!authService.currentUser && to.path !== "/login") {
+        next('/login');
+    } else if (routeRole && (routeRole !== userRole)) {
+        next('/not-found-page');
+    } else if (authService.currentUser && to.path === "/login") {
+        next('/home');
+    } else if (authService.currentUser && !userRole && to.path !== "/register") {
+        next('/register');
+    } else {
+        next();
+    }
+})
+
+export { router }
---
+++ src/routes/routes.js
@@ -0,0 +1,54 @@
+import LoginPage from "@/pages/common/LoginPage";
+import RegistrationPage from "@/pages/common/RegistrationPage";
+import HomePage from "@/pages/buyer/HomePage";
+import BrowsePage from "@/pages/buyer/BrowsePage";
+import HistoryPage from "@/pages/buyer/HistoryPage";
+import SellerHomePage from "@/pages/seller/SellerHomePage";
+import NotFoundPage from "@/pages/common/NotFoundPage";
+
+export const routes = [
+    {
+        path: '/login',
+        component: LoginPage,
+    },
+    {
+        path: '/register',
+        component: RegistrationPage
+    },
+    {
+        path: '/home',
+        component: HomePage,
+        meta: {
+            requiresAuth: true,
+            role: 'buyer'
+        }
+    },
+    {
+        path: '/browse',
+        component: BrowsePage,
+        meta: {
+            requiresAuth: true,
+            role: 'buyer'
+        }
+    },
+    {
+        path: '/history',
+        component: HistoryPage,
+        meta: {
+            requiresAuth: true,
+            role: 'buyer'
+        }
+    },
+    {
+        path: '/seller-home',
+        component: SellerHomePage,
+        meta: {
+            requiresAuth: true,
+            role: 'seller'
+        }
+    },
+    {
+        path: '/not-found-page',
+        component: NotFoundPage,
+    }
+];
