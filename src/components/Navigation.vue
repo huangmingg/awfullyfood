34777dd6e7a1e37a1f7f19d97f5476f26e23a09e
huangminggg <huangming.low@fundingsocieties.com>
2021-03-09 11:30:46 +0800

    Update Navigation.vue

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand to="/">AwfullyFood</b-navbar-brand>
+      <b-navbar-brand to="/seller-home">AwfullyFood</b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item to="/" exact exact-active-class="active">Home</b-nav-item>
+          <b-nav-item to="/home" exact exact-active-class="active">Home</b-nav-item>
           <b-nav-item to="/browse" exact exact-active-class="active">Browse</b-nav-item>
           <b-nav-item to="/history" exact exact-active-class="active">History</b-nav-item>
         </b-navbar-nav>
@@ -17,7 +17,7 @@
               <em>Welcome, Jamie!</em>
             </template>
             <b-dropdown-item to="/">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            <b-dropdown-item v-on:click="signOut">Sign Out</b-dropdown-item>
           </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
@@ -26,8 +26,15 @@
 </template>
 
 <script>
+import { signOut } from "@/services/auth.service";
+
 export default {
-name: "Navigation"
+  name: "Navigation",
+  methods: {
+    signOut: function() {
+      signOut();
+    }
+  }
 }
 </script>
 
