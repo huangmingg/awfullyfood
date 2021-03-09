272b19831b5760bb1e1547777e38a769cf07c872
huangminggg <huangming.low@fundingsocieties.com>
2021-03-09 14:57:43 +0800

    Crea list.service.js

---
+++ src/services/list.service.js
@@ -0,0 +1,30 @@
+// import { database } from "@/firebase";
+// import { store } from "@/stores";
+
+const getListings = async () => {
+}
+
+const getListingBySeller = async (sellerId) => {
+}
+
+const getListing = async (listingId) => {
+}
+
+const createListing = async (payload) => {
+}
+
+const updateListing = async (listingId, payload) => {
+}
+
+const deleteListing = async (listingId) => {
+}
+
+export {
+    getListings,
+    getListingBySeller,
+    getListing,
+    createListing,
+    updateListing,
+    deleteListing
+}
+
