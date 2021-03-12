1b2ac9fb1a94e83b835f3fec798d421b831f2caf
huangminggg <huangming.low@fundingsocieties.com>
2021-03-12 20:31:02 +0800

    Update Navigation.vue
    Removed routing for icon

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand to="/home">AwfullyFood</b-navbar-brand>
+      <b-navbar-brand>AwfullyFood</b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
         <BuyerHeader v-if="role === 'Buyer'"/>
