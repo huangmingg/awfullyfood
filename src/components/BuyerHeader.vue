c4226590e26d9ae9be7088f2f005e602fe4f152c
huangminggg <huangming.low@fundingsocieties.com>
2021-03-12 20:30:47 +0800

    Update BuyerHeader.vue
    Fixed routing path

--- src/components/BuyerHeader.vue
+++ src/components/BuyerHeader.vue
@@ -1,8 +1,8 @@
 <template>
   <b-navbar-nav>
-    <b-nav-item to="/home" exact exact-active-class="active">Home</b-nav-item>
-    <b-nav-item to="/browse" exact exact-active-class="active">Browse</b-nav-item>
-    <b-nav-item to="/history" exact exact-active-class="active">History</b-nav-item>
+    <b-nav-item to="/buyer/home" exact exact-active-class="active">Home</b-nav-item>
+    <b-nav-item to="/buyer/browse" exact exact-active-class="active">Browse</b-nav-item>
+    <b-nav-item to="/buyer/bookmark" exact exact-active-class="active">My Bookmark</b-nav-item>
   </b-navbar-nav>
 </template>
 
