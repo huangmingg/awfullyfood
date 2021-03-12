e35bacb54369654d03b8dd2e1bc9beb5cb6a53f0
huangminggg <huangming.low@fundingsocieties.com>
2021-03-12 20:32:15 +0800

    Create BHomePage.vue

---
+++ src/pages/buyer/BHomePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>Buyer Home Page</div>
+</template>
+
+<script>
+
+import { getUserProfile } from "@/services/user.service";
+import { authService } from "@/firebase";
+import { store } from "@/stores";
+
+export default {
+  name: "BHomePage",
+  async created() {
+    if (!store.getters.getProfileState) {
+      await getUserProfile(authService.currentUser.uid);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
