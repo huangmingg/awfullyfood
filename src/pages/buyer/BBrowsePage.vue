df1975d95c7d087035238f50a6d137aa7c62aad1
huangminggg <huangming.low@fundingsocieties.com>
2021-03-13 11:54:35 +0800

    Update BBrowsePage

--- src/pages/buyer/BBrowsePage.vue
+++ src/pages/buyer/BBrowsePage.vue
@@ -1,10 +1,51 @@
 <template>
-  <div>Browsing page</div>
+  <b-card-group deck>
+    <b-card
+        v-for="list in listing"
+        v-bind:key="list.id"
+        :title=list.name
+        :img-src=list.imageURL
+        img-alt="Image"
+        img-top
+        img-height="200"
+        img-width="150"
+        style="max-width: 20rem;"
+        class="mb-2"
+        border-variant="primary"
+
+    >
+      <b-card-text>
+        {{ list.description }}
+      </b-card-text>
+
+      <b-button variant="primary" v-on:click="navigate(list.id)">
+        View More
+      </b-button>
+    </b-card>
+  </b-card-group>
 </template>
 
 <script>
+import { getListings } from "@/services/list.service";
+import { store } from "@/stores";
+import { router } from "@/routes";
+
 export default {
-name: "BBrowsePage"
+  name: "BBrowsePage",
+  computed: {
+    listing() {
+      return store.getters.getList;
+    },
+  },
+  created() {
+    getListings();
+  },
+
+  methods: {
+    navigate: function (listId) {
+      router.push(`browse/${listId}`);
+    }
+  }
 }
 </script>
 
