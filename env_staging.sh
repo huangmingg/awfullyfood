781eca6d5277cdb678e1bbcb8d867b23c1c979a6
huangminggg <huangming.low@fundingsocieties.com>
2021-03-13 17:36:50 +0800

    Update env_staging.sh

--- env_staging.sh
+++ env_staging.sh
@@ -1,9 +1,2 @@
-ECHO "export const firebaseConfig = {
-  apiKey: '$APIKEY',
-  authDomain: 'awfullyfood-f3e55.firebaseapp.com',
-  projectId: 'awfullyfood-f3e55',
-  storageBucket: 'awfullyfood-f3e55.appspot.com',
-  messagingSenderId: '$MSID',
-  appId: '$APPID',
-};" > src/environments/environment.staging.js
+ECHO "testing" > src/environments/environment.staging.js
 
