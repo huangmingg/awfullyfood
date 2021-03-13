bbe4044521a397dc6187b80d84cdfc27681d3a14
huangminggg <huangming.low@fundingsocieties.com>
2021-03-13 17:10:41 +0800

    Create env_staging.sh

---
+++ env_staging.sh
@@ -0,0 +1,10 @@
+CAT <<EOT > src/environments/environment.staging.js
+export const firebaseConfig = {
+  apiKey: "$APIKEY",
+  authDomain: "awfullyfood-f3e55.firebaseapp.com",
+  projectId: "awfullyfood-f3e55",
+  storageBucket: "awfullyfood-f3e55.appspot.com",
+  messagingSenderId: "$MSID",
+  appId: "$APPID",
+};
+EOT
