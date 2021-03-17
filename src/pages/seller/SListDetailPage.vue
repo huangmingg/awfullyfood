a081ea90dddb50a2c51940a87b42529fb01c9d58
huangminggg <huangming.low@fundingsocieties.com>
2021-03-17 11:10:08 +0800

    Create SListDetailPage.vue

---
+++ src/pages/seller/SListDetailPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-button v-on:click="back()">Back </b-button>
+    List Detail Page
+  </div>
+</template>
+
+<script>
+import { router } from "@/routes";
+
+export default {
+  name: "SListPage",
+  computed: {
+  },
+  async created() {
+  },
+  methods: {
+    back: function() {
+      router.back();
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
