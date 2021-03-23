a2b5460061a8e20b7dd7edc14afc25e1b858abf3
huangminggg <huangming.low@fundingsocieties.com>
2021-03-23 12:19:29 +0800

    Update ProfileHeader.vue

--- src/components/ProfileHeader.vue
+++ src/components/ProfileHeader.vue
@@ -4,7 +4,7 @@
       <template #button-content>
         <em>Welcome, {{ name }}!</em>
       </template>
-      <b-dropdown-item to="/profile">Profile</b-dropdown-item>
+      <b-dropdown-item to="/profile">My Profile</b-dropdown-item>
       <b-dropdown-item to="/search">Search User</b-dropdown-item>
       <b-dropdown-item v-on:click="signOut()">Sign Out</b-dropdown-item>
     </b-nav-item-dropdown>
