0b14ad55fc3c7e23148da1115e3c3428b1afbe9e
huangminggg <huangming.low@fundingsocieties.com>
2021-03-23 12:19:43 +0800

    Update SearchUserPage.vue

--- src/pages/common/SearchUserPage.vue
+++ src/pages/common/SearchUserPage.vue
@@ -1,45 +1,67 @@
 <template>
     <b-container fluid class="p-4 bg-light">
-      <b-row class="my-5">
-          <b-img class="border-info profile-photo" thumbnail width=400 height=400 fluid :src="photo" alt="Display Photo">
-          </b-img>
-        <b-col>
+        <b-row class="mx-2">
+          <b-input-group>
+            <b-form-input
+                v-model="userId"
+                placeholder="Enter User ID">
+            </b-form-input>
+            <b-button class="ml-2" variant="outline-info" @click="submitParam()">Search</b-button>
+          </b-input-group>
+        </b-row>
+      <hr/>
+      <b-collapse id="collapse-1" class="mt-2">
+        <b-row class="my-5" v-show="found == false">
           <b-card-text>
-            <b-form-rating v-model="averageRating" readonly show-value precision="2" inline></b-form-rating>
+            User Not Found, please try again!
           </b-card-text>
-          <b-card-text>
-            Name: {{ this.profile.name }}
-          </b-card-text>
-          <b-card-text>
-            Role: {{ this.profile.role }}
-          </b-card-text>
-          <b-card-text>
-            Number: {{ this.profile.phoneNumber }}
-          </b-card-text>
-          <b-card-text>
-            Address: {{ this.profile.address }}
-          </b-card-text>
-        </b-col>
-      </b-row>
-        <b-card-title>
-          Recent Reviews
-        </b-card-title>
-        <b-list-group >
-          <b-list-group-item
-              v-for="(review, index) in reviews"
-              v-bind:key="index"
-              class="flex-column align-items-start list-item"
-          >
-            <div class="d-flex w-100 justify-content-between">
-              <p class="mb-1 font-italic"> "{{ review.description }}" </p>
-              <small>
-                <b-form-rating :value="review.rating" readonly precision="2" inline></b-form-rating>
-              </small>
-            </div>
-            <small> {{ review.username }}, {{ review.updatedAt.toDate().toLocaleDateString() }} </small>
-          </b-list-group-item>
-        </b-list-group>
+        </b-row>
 
+        <div v-show="found">
+          <b-row class="my-5">
+              <b-img class="border-info profile-photo" thumbnail width=400 height=400 fluid :src="photo" alt="Display Photo">
+              </b-img>
+            <b-col>
+              <b-card-text>
+                <b-form-rating v-model="averageRating" readonly show-value precision="2" inline></b-form-rating>
+              </b-card-text>
+              <b-card-text>
+                Name: {{ this.profile.name }}
+              </b-card-text>
+              <b-card-text>
+                Role: {{ this.profile.role }}
+              </b-card-text>
+              <b-card-text>
+                Number: {{ this.profile.phoneNumber }}
+              </b-card-text>
+              <b-card-text>
+                Address: {{ this.profile.address }}
+              </b-card-text>
+            </b-col>
+          </b-row>
+          <b-card-title>
+            Recent Reviews
+          </b-card-title>
+          <b-list-group>
+            <b-list-group-item
+                v-for="(review, index) in reviews"
+                v-bind:key="index"
+                class="flex-column align-items-start list-item"
+            >
+              <div class="d-flex w-100 justify-content-between">
+                <p class="mb-1 font-italic"> "{{ review.description }}" </p>
+                <small>
+                  <b-form-rating :value="review.rating" readonly precision="2" inline></b-form-rating>
+                </small>
+              </div>
+              <small> {{ review.username }}, {{ review.updatedAt.toDate().toLocaleDateString() }} </small>
+            </b-list-group-item>
+            <b-card-text v-show="!reviews.length">
+              Oops there are no reviews for this user yet, check back later!
+            </b-card-text>
+          </b-list-group>
+        </div>
+      </b-collapse>
     </b-container>
 </template>
 
@@ -47,7 +69,9 @@
 
 import { getUserProfile } from "@/services/user.service";
 import { getReviews, getAggregatedRating } from "@/services/review.service";
-// import { getDisplayPhoto } from "@/services/user.service";
+import { getDisplayPhoto } from "@/services/user.service";
+import { isEmptyObject } from "@/services/utils.service";
+import { router } from "@/routes";
 
 export default {
   name: "ProfilePage",
@@ -58,31 +82,72 @@ export default {
       profile: {},
       reviews: [],
       photo: '',
+      found: false,
+      click: false,
     }
-  },
-  created() {
-    this.userId = this.$route.params.id;
   },
   async mounted() {
-    this.profile = await getUserProfile(this.userId, false);
-    // this.photo = await getDisplayPhoto(this.userId);
-    this.photo = "http://picsum.photos/400/600/?image=82"
-    this.reviews = await getReviews(this.userId, this.profile?.role);
-    for (const review of this.reviews) {
-      review.username = await this.getDisplayName(review.userId);
-    }
-    this.averageRating = getAggregatedRating(this.reviews);
+    this.userId = this.$route.query.userId;
+    this.userId ? await this.searchUser() : null;
   },
+
+  watch: {
+    '$route.params': async function() {
+      await this.searchUser();
+    }
+  },
+
   methods: {
     getDisplayName: async function(userId) {
       const res = await getUserProfile(userId);
       return res.name;
+    },
+
+    searchUser: async function() {
+      if (!this.userId) {
+        alert("Please key in user ID you wish to search...");
+        return;
+      }
+
+      if (!this.click) {
+        this.click = true;
+        this.$root.$emit('bv::toggle::collapse', 'collapse-1');
+      }
+
+      this.profile = await getUserProfile(this.userId, false);
+      this.found = !isEmptyObject(this.profile);
+      if (this.found) {
+        this.photo = await getDisplayPhoto(this.userId);
+        if (!this.photo) {
+          this.photo = await getDisplayPhoto('dummy.png');
+        }
+        this.reviews = await getReviews(this.userId, this.profile?.role);
+        for (const review of this.reviews) {
+          review.username = await this.getDisplayName(review.userId);
+        }
+        this.averageRating = getAggregatedRating(this.reviews);
+      }
+    },
+
+    submitParam: function() {
+      this.userId = this.sanitizeQuery(this.userId);
+      const currentRoute = router.currentRoute.query.userId;
+      if (currentRoute != this.userId) {
+        router.replace({
+          query: { userId: this.userId },
+        });
+      }
+    },
+
+    sanitizeQuery: function(userId) {
+      return userId.trim();
     }
   }
 }
 </script>
 
 <style scoped>
+
 profile-photo {
   max-width: 400px;
   max-height: 400px;
