a1b8ed1d376eae800b5c3187128a7a36aac00ad3
edologgerbird <E0406924@u.nus.edu>
2021-03-29 14:21:11 +0800

    Update STopLikes.vue

--- src/pages/seller/visualisation/STopLikes.vue
+++ src/pages/seller/visualisation/STopLikes.vue
@@ -33,7 +33,7 @@ export default {
   methods: {
     route: function (event) {
       var userId = event.target.getAttribute("id");
-      this.$router.push({ path: `/buyer/browse/${userId}` });
+      this.$router.push({ path: `/seller/list/${userId}` });
     },
   },
   components: {},
