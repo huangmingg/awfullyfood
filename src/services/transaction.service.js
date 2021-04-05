0a7e9bddbd2651d1d4c491295740cbfdbccdbeec
maviseu <mavchong99@gmail.com>
2021-04-05 15:11:54 +0800

    Update transaction.service.js

--- src/services/transaction.service.js
+++ src/services/transaction.service.js
@@ -1,16 +1,17 @@
 import { database } from "@/firebase";
 import { store } from "@/stores";
+import { TransactionCreate, TransactionRead, TransactionUpdate } from "@/models/transaction.class";
+import { getCurrentTimestamp } from "@/services/utils.service";
+import { Review } from "@/models/review.class";
+import { getListingName } from "@/services/list.service";
 
 const getTransactions = async (saveState = true) => {
     return database.collection("transactions").get()
         .then(async (res) => {
-            const output = res.docs.map(doc => {
-                return {
-                    ...doc.data(),
-                    'id': doc.id,
-                };
-            });
-            saveState ? await store.dispatch('updateList', output) : null;
+            const output = await Promise.all(res.docs.map(async(doc) => {
+                return new TransactionRead(doc.data(), doc.id, await getListingName(doc.data()?.listingId))
+            }));
+            saveState ? await store.dispatch('updateTransaction', output) : null;
             return output;
         })
         .catch((error) => {
@@ -24,13 +25,10 @@ const getTransactionsBySeller = async (sellerId, saveState = true) => {
         .where("sellerId", "==", sellerId)
         .get()
         .then(async (res) => {
-            const output = res.docs.map(doc => {
-                return {
-                    ...doc.data(),
-                    'id': doc.id,
-                };
-            });
-            saveState ? await store.dispatch('updateList', output) : null;
+            const output = await Promise.all(res.docs.map(async(doc) => {
+                return new TransactionRead(doc.data(), doc.id, await getListingName(doc.data()?.listingId))
+            }));
+            saveState ? await store.dispatch('updateTransaction', output) : null;
             return output;
         })
         .catch((error) => {
@@ -44,13 +42,10 @@ const getTransactionsByBuyer = async (buyerId, saveState = true) => {
         .where("buyerId", "==", buyerId)
         .get()
         .then(async (res) => {
-            const output = res.docs.map(doc => {
-                return {
-                    ...doc.data(),
-                    'id': doc.id,
-                };
-            });
-            saveState ? await store.dispatch('updateList', output) : null;
+            const output = await Promise.all(res.docs.map(async(doc) => {
+                return new TransactionRead(doc.data(), doc.id, await getListingName(doc.data()?.listingId))
+            }));
+            saveState ? await store.dispatch('updateTransaction', output) : null;
             return output;
         })
         .catch((error) => {
@@ -76,8 +71,22 @@ const getTransactionsByListing = async (listingId, saveState = false) => {
         });
 }
 
-const approveTransaction = async (transactionId) => {
-    return database.collection("transactions").doc(transactionId).update({isApproved:true})
+const createTransaction = async (payload) => {
+    const transaction = new TransactionCreate(payload);
+    return database.collection("transactions").add(transaction)
+        .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+            return true;
+        })
+        .catch((error) => {
+            console.log(error);
+            return false;
+        });
+}
+
+const updateTransaction = async (transactionId, payload) => {
+    const transaction = new TransactionUpdate(payload);
+    return database.collection("transactions").doc(transactionId).update(transaction)
         .then(() => {
             return true;
         })
@@ -87,9 +96,35 @@ const approveTransaction = async (transactionId) => {
         });
 }
 
+const deleteTransaction = async (transactionId) => {
+    const deletePayload = { 'deletedAt': getCurrentTimestamp() };
+    return await updateTransaction(transactionId, deletePayload);
+}
+
+const approveTransaction = async (transactionId) => {
+    const approvePayload = { 'isApproved': true };
+    return await updateTransaction(transactionId, approvePayload);
+}
+
+const updateBuyerReview = async (transactionId, rating, description) => {
+    const reviewPayload = new Review(rating, description);
+    return await updateTransaction(transactionId, { 'buyerReview': reviewPayload });
+}
+
+const updateSellerReview = async (transactionId, rating, description) => {
+    const reviewPayload = new Review(rating, description);
+    return await updateTransaction(transactionId, { 'sellerReview': reviewPayload });
+}
+
 export {
     getTransactions,
     getTransactionsByBuyer,
     getTransactionsByListing,
     getTransactionsBySeller,
+    createTransaction,
+    updateTransaction,
+    deleteTransaction,
+    approveTransaction,
+    updateBuyerReview,
+    updateSellerReview,
 }
