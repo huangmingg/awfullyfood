8a32d574912a55eea8621101dff0d8295655f601
huangmingg <huangming.low@fundingsocieties.com>
2021-04-05 10:36:02 +0800

    Update listing.class.js

--- src/models/listing.class.js
+++ src/models/listing.class.js
@@ -1,21 +1,48 @@
-import Metadata from '@/models/metadata.class'
-
-class Listing {
-    constructor(obj) {
-        this.id = obj.id;
-        this.name = obj.name;
-        this.description = obj.description;
-        this.price = obj.price;
-        this.quantity = obj.quantity;
-        this.unit = obj.unit;
-        this.category = obj.category;
-        this.status = obj.status;
-        this.createdAt = obj.createdAt;
-        this.expiredAt = obj.expiredAt;
-        this.sellerId = obj.sellerId;
-        this.likes = new Metadata(obj.likes);
-        this.interests = new Metadata(obj.interests);
+export class ListingCreate {
+    constructor(list) {
+        this.sellerId = list.sellerId;
+        this.name = list.name;
+        this.description = list.description;
+        this.price = list.price;
+        this.quantity = list.quantity;
+        this.unit = list.unit;
+        this.category = list.category;
+        this.status = list.status;
+        this.bookmarks = list.bookmarks ? list.bookmarks : [];
+        this.interests = list.interests ? list.interests : [];
+        this.createdAt = list.createdAt;
+        this.expiredAt = list.expiredAt;
+        this.deletedAt = list.deletedAt;
     }
 }
 
-module.exports = Listing;
+export class ListingRead extends ListingCreate {
+    constructor(list, id, photo) {
+        super(list);
+        this.id = id;
+        this.photo = photo;
+    }
+}
+
+export class ListingUpdate {
+    constructor(obj) {
+        const getData = data => ({
+            ...data.sellerId  && { sellerId : data.sellerId },
+            ...data.name && { name: data.name },
+            ...data.description && { description: data.description },
+            ...data.price && { price: data.price },
+            ...data.quantity && { quantity: data.quantity },
+            ...data.unit && { unit: data.unit },
+            ...data.category && { category: data.category },
+            ...data.status && { status: data.status },
+            ...data.bookmarks && { bookmarks: data.bookmarks },
+            ...data.interests && { interests: data.interests },
+            ...data.createdAt && { createdAt: data.createdAt },
+            ...data.expiredAt && { expiredAt: data.expiredAt },
+            ...data.deletedAt && { deletedAt: data.deletedAt },
+        });
+        for (const [key, value] of Object.entries(getData(obj))) {
+            this[key] = value;
+        };
+    }
+}
