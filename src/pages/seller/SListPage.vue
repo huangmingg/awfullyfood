d24fc55b341634cf7507558d2b434a017a3dbbf1
huangmingg <huangming.low@fundingsocieties.com>
2021-04-09 12:01:55 +0800

    Update SListPage.vue

--- src/pages/seller/SListPage.vue
+++ src/pages/seller/SListPage.vue
@@ -29,38 +29,36 @@
 
     <hr class="dropdown-divider">
     <b-row>
-      <b-row>
-        <b-card
-          v-for="list in listing"
-          :key="list.id"
-          :title="list.name"
-          :img-src="list.photo"
-          img-alt="Image"
-          img-top
-          img-height="200"
-          img-width="150"
-          style="max-width: 20rem"
-          class="m-3 list-item"
-          border-variant="info"
-          @click="edit(list.id)"
-        >
-          <b-card-text>
-            {{ list.description }}
-            <br>
-            ${{ list.price }} per {{ list.unit }}
-            <br>
-            <small>Created Date:
-              {{ convertTimestamp(list.createdAt) }}</small>
-            <br>
-            <small>Expiry Date:
-              {{ convertTimestamp(list.expiredAt) }}</small>
-          </b-card-text>
-          <b-icon-heart-fill style="color: red" />
-          <span>
-            {{ list.bookmarks.length }}
-          </span>
-        </b-card>
-      </b-row>
+      <b-card
+        v-for="list in listing"
+        :key="list.id"
+        :title="list.name"
+        :img-src="list.photo"
+        img-alt="Image"
+        img-top
+        img-height="250px"
+        img-width="150px"
+        style="max-width: 20rem"
+        class="m-3 list-item"
+        border-variant="info"
+        @click="edit(list.id)"
+      >
+        <b-card-text>
+          ${{ list.price }} per {{ list.unit }}
+          <br>
+          {{ list.description }}
+          <br>
+          <small>Created Date:
+            {{ convertTimestamp(list.createdAt) }}</small>
+          <br>
+          <small>Expiry Date:
+            {{ convertTimestamp(list.expiredAt) }}</small>
+        </b-card-text>
+        <b-icon-heart-fill style="color: red" />
+        <span>
+          {{ list.bookmarks.length }}
+        </span>
+      </b-card>
     </b-row>
   </div>
 </template>
@@ -134,27 +132,10 @@ export default {
 </script>
 
 <style scoped>
+
 .list-item:hover {
   background-color: rgb(243, 250, 251);
   cursor: pointer;
 }
-
-.my-grid {
-  display: grid;
-  justify-items: center;
-  /* 280px is the minimum a column can get, you might need to adjust it based on your needs. */
-  grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
-  grid-gap: 1.5rem;
-}
-
-.my-grid > * {
-  width: 100%;
-  max-width: 20rem;
-}
-
-.card-columns {
-  column-gap: 0.0rem;
-}
-
 
 </style>
