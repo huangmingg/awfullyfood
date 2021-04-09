36ebe784a78e29c154a39fac74571630c996f765
huangmingg <huangming.low@fundingsocieties.com>
2021-04-09 10:58:32 +0800

    Update SListPage.vue

--- src/pages/seller/SListPage.vue
+++ src/pages/seller/SListPage.vue
@@ -28,38 +28,40 @@
     </b-button-group>
 
     <hr class="dropdown-divider">
-    <b-card-group deck>
-      <b-card
-        v-for="list in listing"
-        :key="list.id"
-        :title="list.name"
-        :img-src="list.photo"
-        img-alt="Image"
-        img-top
-        img-height="200"
-        img-width="150"
-        style="max-width: 20rem"
-        class="mb-2 list-item"
-        border-variant="info"
-        @click="edit(list.id)"
-      >
-        <b-card-text>
-          {{ list.description }}
-          <br>
-          ${{ list.price }} per {{ list.unit }}
-          <br>
-          <small>Created Date:
-            {{ convertTimestamp(list.createdAt) }}</small>
-          <br>
-          <small>Expiry Date:
-            {{ convertTimestamp(list.expiredAt) }}</small>
-        </b-card-text>
-        <b-icon-heart-fill style="color: red" />
-        <span style="color: red">
-          {{ list.bookmarks.length }}
-        </span>
-      </b-card>
-    </b-card-group>
+    <b-row>
+      <b-row>
+        <b-card
+          v-for="list in listing"
+          :key="list.id"
+          :title="list.name"
+          :img-src="list.photo"
+          img-alt="Image"
+          img-top
+          img-height="200"
+          img-width="150"
+          style="max-width: 20rem"
+          class="m-3 list-item"
+          border-variant="info"
+          @click="edit(list.id)"
+        >
+          <b-card-text>
+            {{ list.description }}
+            <br>
+            ${{ list.price }} per {{ list.unit }}
+            <br>
+            <small>Created Date:
+              {{ convertTimestamp(list.createdAt) }}</small>
+            <br>
+            <small>Expiry Date:
+              {{ convertTimestamp(list.expiredAt) }}</small>
+          </b-card-text>
+          <b-icon-heart-fill style="color: red" />
+          <span>
+            {{ list.bookmarks.length }}
+          </span>
+        </b-card>
+      </b-row>
+    </b-row>
   </div>
 </template>
 
@@ -136,5 +138,23 @@ export default {
   background-color: rgb(243, 250, 251);
   cursor: pointer;
 }
+
+.my-grid {
+  display: grid;
+  justify-items: center;
+  /* 280px is the minimum a column can get, you might need to adjust it based on your needs. */
+  grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
+  grid-gap: 1.5rem;
+}
+
+.my-grid > * {
+  width: 100%;
+  max-width: 20rem;
+}
+
+.card-columns {
+  column-gap: 0.0rem;
+}
+
 
 </style>
