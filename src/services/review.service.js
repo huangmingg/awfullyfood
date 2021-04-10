e3c41efde86e4db11c82ceb035ab7feca909cbdb
huangmingg <huangming.low@fundingsocieties.com>
2021-04-10 19:27:13 +0800

    Update review.service.js

--- src/services/review.service.js
+++ src/services/review.service.js
@@ -1,24 +1,27 @@
 import { getTransactionsBySeller, getTransactionsByBuyer } from '@/services/transaction.service';
 import { getDisplayName } from '@/services/user.service';
+import { isEmptyObject } from '@/services/utils.service';
 
 const getReviews = async (userId, role) => {
   const key = role === 'Buyer' ? 'buyerReview' : 'sellerReview';
   const transactions = role === 'Buyer' ? await getTransactionsByBuyer(userId) : await getTransactionsBySeller(userId);
   return Promise.all(
-    transactions.map(async (ele) => {
-      return {
-        ...ele[key],
-        username: role === 'Buyer' ? await getDisplayName(ele.sellerId) : await getDisplayName(ele.buyerId),
-        userId: role === 'Buyer' ? ele.sellerId : ele.buyerId,
-      };
-    })
+    transactions
+      .filter((ele) => { return role === 'Buyer' ? !isEmptyObject(ele.buyerReview) : !isEmptyObject(ele.sellerReview) ; })
+      .map(async (ele) => {
+        return {
+          ...ele[key],
+          username: role === 'Buyer' ? await getDisplayName(ele.sellerId) : await getDisplayName(ele.buyerId),
+          userId: role === 'Buyer' ? ele.sellerId : ele.buyerId,
+        };
+      })
   );
 };
 
 const getAggregatedRating = (reviews) => {
   const length = reviews.length;
   if (length) {
-    const total = reviews.map((ele) => { return ele.rating; }).reduce((a, b) => { return a + b, 0; });
+    const total = reviews.map((ele) => { return ele.rating ? ele.rating : 0; }).reduce((a, b) => { return a + b, 0; });
     return (total / length);
   }
   return 0;
